66784e94b9d0627eee4169c9d0302cfdd58689ef
raji-chitra <rajalakshmim.23it@kongu.edu>
2025-08-16 12:32:26 +0530

    navigationbar 2025-08-16

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home' 
-import About from './pages/About'
-import Contact from './pages/contact'
+// ...existing code...
+import Contact from './pages/Contact'
 import Login from './pages/Login'
 import Myprofile from './pages/Myprofile'
 import MyAppointments from './pages/MyAppointments'
 import Doctors from './pages/Doctors' 
-import
+import About from './pages/About'
+import Appointment from './pages/Appointment'
+
+
+import { Navbar } from './components/Navbar'
+// ...existing code...
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
+         <Route path='/doctors' element={<Doctors />} />
+          <Route path='/doctors/:speciality' element={<Doctors />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/myprofile' element={<Myprofile />} />
-        <Route path='/myappointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<Myprofile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} /> 
+         <Route path='/appointment/:docId' element={<Appointment/>} />
       </Routes>
     </div>
   )
